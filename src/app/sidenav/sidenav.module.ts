105d18ec66f4d8d4bd319ec776cca0cce227a584
T3130 <wadsf506470@gmail.com>
2019-12-12 11:26:30 +0800

    Layout component add Sidenav component

--- src/app/sidenav/sidenav.module.ts
+++ src/app/sidenav/sidenav.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidenavComponent } from './sidenav.component';
+import { RouterModule } from '@angular/router';
+import { MaterialModule } from 'src/@happy/shared/material-components.module';
+import { SidenavItemComponent } from './sidenav-item/sidenav-item.component';
 
 
 
 @NgModule({
-  declarations: [SidenavComponent],
+  declarations: [SidenavComponent, SidenavItemComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule,
+    MaterialModule
+  ],
+  exports: [SidenavComponent]
 })
 export class SidenavModule { }
