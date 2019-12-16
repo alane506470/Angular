a166437f9335c82db543fd216a9ab90d41b60a24
T3130 <wadsf506470@gmail.com>
2019-12-16 10:10:30 +0800

    利用廣播動態變更動畫、下拉menu的動畫

--- src/app/sidenav/sidenav-item/sidenav-item.component.ts
+++ src/app/sidenav/sidenav-item/sidenav-item.component.ts
@@ -1,20 +1,55 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { SidenavItem } from '../sidenav-item.interface';
 import isFunction from 'lodash-es/isFunction';
+import { Observable } from 'rxjs';
+import { SidenavService } from '../sidenav.service';
+import { map } from 'rxjs/operators';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidenav-item',
   templateUrl: './sidenav-item.component.html',
-  styleUrls: ['./sidenav-item.component.scss']
+  styleUrls: ['./sidenav-item.component.scss'],
+  animations: [
+    trigger('dropdownOpen', [
+      state('false', style({
+        height: 0
+      })),
+      state('true', style({
+        height: '*'
+      })),
+      transition('false <=> true', animate('300ms cubic-bezier(.35, 0, .25, 1)'))
+    ])
+  ]
 })
 export class SidenavItemComponent implements OnInit {
 
-  @Input('item') item : SidenavItem;
+  @Input('item') item: SidenavItem;
 
-  @Input('level') level:number;
-  constructor() { }
+  @Input('level') level: number;
+
+  dropdownOpen$: Observable<boolean>;
+  constructor(private sidenavService: SidenavService, private router: Router) {
+    this.dropdownOpen$ = this.sidenavService.currentlyOpen$.pipe(
+      map(currentlyOpen => this.item.subItems && this.item.subItems.length > 0 && currentlyOpen.indexOf(this.item) > -1)
+    );
+
+  }
 
   ngOnInit() {
+  }
+
+  handleClick() {
+    if (this.item.subItems && this.item.subItems.length > 0) {
+      this.sidenavService.toggleItemOpen(this.item);
+    } else if (typeof this.item.routeOrFunction === 'string' || this.item.routeOrFunction instanceof String) {
+      this.router.navigate([this.item.routeOrFunction]);
+    } else if (typeof this.item.routeOrFunction === 'function' || this.item.routeOrFunction instanceof Function) {
+      this.item.routeOrFunction();
+    } else {
+      throw Error('Could not determine what to do, Sidenav-Item has no routeOrFunction set AND does not contain any subItems');
+    }
   }
 
   isFunction(routeOrFunction: string[] | Function) {
